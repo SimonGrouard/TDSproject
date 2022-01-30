cb3bcebc72b9a58ea0d338ce7ab537b340beb493
Sonic101SEA <st121@ic.ac.uk>
2022-01-30 10:34:35 +0000

    Deleted test text file and added code for descriptive analysis

--- scripts/descriptive_analysis.R
+++ scripts/descriptive_analysis.R
@@ -1,4 +1,11 @@
+rm(list=ls())
+
+# Libraries
+library(data.table)
+
+# Data
 mydata <- fread("/rds/general/project/hda_21-22/live/TDS/General/Data/ukb47946.csv", data.table=FALSE, nrows=500, select=1:1000)
+data_extract <- readRDS("/rds/general/project/hda_21-22/live/TDS/Group_6/extraction_and_recording/outputs/ukb_extracted.rds")
 
 # Reading arguments: For sh script to load data in future
 args <- commandArgs(trailingOnly = TRUE)
